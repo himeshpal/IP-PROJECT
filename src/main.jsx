7bc78f484d5b7b6e73921524fbcc4f33c1c28942
himeshpal <155574981+himeshpal@users.noreply.github.com>
2025-02-17 01:15:01 +0530

    chnaged to reddit user

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import GitHubFinder from "./GitHubFinder.jsx";
+import RedditFeedViewer from "./RedditFeedViewer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GitHubFinder />
+    <RedditFeedViewer />
   </StrictMode>
 );
